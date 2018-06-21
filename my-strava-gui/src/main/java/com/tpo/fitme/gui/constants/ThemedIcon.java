0f41fb3b93a60d10fc21ad22e7ad5dcd0d370833
Tiberiu-Biking <tiberiu.popa@gmail.com>
2018-06-21 17:56:33 +0200

    Themed icon

---
+++ my-strava-gui/src/main/java/com/tpo/fitme/gui/constants/ThemedIcon.java
@@ -0,0 +1,23 @@
+package com.tpo.fitme.gui.constants;
+
+import com.vaadin.server.ThemeResource;
+import lombok.Getter;
+
+/**
+ * @author Tiberiu
+ * @since 19.06.18
+ */
+@Getter
+public enum ThemedIcon {
+
+    ROAD("img/road.png"),
+    HIKE("img/hike.png"),
+    SKI("img/ski.png"),
+    MTB("img/mtb.png");
+
+    private final ThemeResource resource;
+
+    ThemedIcon(String themedResourceId) {
+        resource = new ThemeResource(themedResourceId);
+    }
+}

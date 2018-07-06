9758a8f6a1f5f52c7a9e4b0db71099f549ad2d60
Tiberiu-Biking <tiberiu.popa@gmail.com>
2018-07-06 16:22:05 +0200

    Activity Grid improvments

--- my-strava-gui/src/main/java/com/tpo/fitme/gui/view/grid/ActivityGridView.java
+++ my-strava-gui/src/main/java/com/tpo/fitme/gui/view/grid/ActivityGridView.java
@@ -6,6 +6,11 @@ import com.tpo.fitness.service.activity.ActivitiesService;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.ui.Grid;
 
+import java.time.format.FormatStyle;
+
+import static java.time.format.DateTimeFormatter.ofLocalizedDateTime;
+import static org.apache.commons.lang3.time.DurationFormatUtils.formatDuration;
+
 /**
  * @author Tiberiu
  * @since 14.10.17
@@ -26,11 +31,28 @@ public class ActivityGridView extends AbstractActivitiesView {
         grid.setItems(activitiesService.getAllInChronologicalOrder());
         grid.addColumn(Activity::getName).setCaption("Name").setHidable(false);
         grid.addColumn(Activity::getSport).setCaption("Type").setHidable(false);
-        grid.addColumn(Activity::getCalories).setCaption("Calories").setHidable(false);
-        grid.addColumn(Activity::getDuration).setCaption("Duration(minutes)").setHidable(false);
-        grid.addColumn(Activity::getDistance).setCaption("Distance(km)").setHidable(true);
-        grid.addColumn(Activity::getElevation).setCaption("Elevation(meters)").setHidable(true);
-        grid.addColumn(Activity::getStartDate).setCaption("Date").setHidable(true);
+
+        grid.addColumn(activity -> activity.getStartDate().format(ofLocalizedDateTime(FormatStyle.MEDIUM)))
+                .setCaption("Date")
+                .setHidable(true);
+
+        grid.addColumn(activity -> activity.getCalories() + " kcal")
+                .setCaption("Calories")
+                .setHidable(false);
+
+        grid.addColumn(activity -> formatDuration(activity.getDuration() * 60 * 1000, "HH:mm:ss"))
+                .setCaption("Duration")
+                .setHidable(false);
+
+        grid.addColumn(activity -> activity.getDistance().longValue() + " km")
+                .setCaption("Distance")
+                .setHidable(true);
+
+        grid.addColumn(activity -> activity.getElevation().longValue() + " m")
+                .setCaption("Elevation")
+                .setHidable(true);
+
+
         return grid;
     }
 
